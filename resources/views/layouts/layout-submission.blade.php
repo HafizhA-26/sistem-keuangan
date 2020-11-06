1f493e9ff5ea0deaa9490d2f2cc169a5640b8591
Septian Yoga <septianpermadi0709@gmail.com>
2020-11-06 15:04:40 +0700

    Layout Dashboard dan Submission

--- resources/views/layouts/layout-submission.blade.php
+++ resources/views/layouts/layout-submission.blade.php
@@ -9,9 +9,9 @@
 					<span class="h6">&nbsp;Sistem Informasi Keuangan</span>
 				</header>
 				<ul class="">
-					<li class="list-inline h4"><a href="index-kepsek.php" class="nav-link listtype">Dashboard</a></li><br>
+					<li class="list-inline h4"><a href="" class="nav-link listtype">Dashboard</a></li><br>
 					<li class="list-inline h4"><a href="" class="nav-link disabled listtype">Submission</a></li><br>
-					<li class="list-inline h4"><a href="report.php" class="nav-link listtype">Report</a></li><br>
+					<li class="list-inline h4"><a href=" class="nav-link listtype">Report</a></li><br>
 					<li class="list-inline h4"><a href="" class="nav-link listtype">Add Account</a></li><br>
 					<li class="list-inline h5 bawah"><a href="login.php" class="listtype">Sign Out <i class="fa fa-sign-out-alt"></i></a></li>
                 </ul>
