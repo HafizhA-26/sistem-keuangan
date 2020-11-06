bcc05c4d65effd4d54a69549d40f990b606bd498
Septian Yoga <septianpermadi0709@gmail.com>
2020-11-06 13:56:59 +0700

    Perbaikan Font Awesome Library

--- resources/views/layouts/layout1.blade.php
+++ resources/views/layouts/layout1.blade.php
@@ -5,6 +5,7 @@
         <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <link href="css/all.min.css" type="text/css" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
         <link rel="icon" type="image/png" href="img/icon/stm.png"/>
         <!-- Cek file controller untuk menambahkan title dinamis di app/Http/...... --> 
         <title>{{ $title }}</title>
