f7cfc23ea425542299b24e179939fbab2ecd3a48
M Hafizh Auliansyah <hafizh.auliansyah@gmail.com>
2020-11-10 04:12:53 +0700

    Menambahkan Intervention Image pada laravel

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -60,6 +60,7 @@ return array(
     'League\\CommonMark\\' => array($vendorDir . '/league/commonmark/src'),
     'Laravel\\Ui\\' => array($vendorDir . '/laravel/ui/src'),
     'Laravel\\Tinker\\' => array($vendorDir . '/laravel/tinker/src'),
+    'Intervention\\Image\\' => array($vendorDir . '/intervention/image/src/Intervention/Image'),
     'Illuminate\\Support\\' => array($vendorDir . '/laravel/framework/src/Illuminate/Macroable', $vendorDir . '/laravel/framework/src/Illuminate/Collections'),
     'Illuminate\\Foundation\\Auth\\' => array($vendorDir . '/laravel/ui/auth-backend'),
     'Illuminate\\' => array($vendorDir . '/laravel/framework/src/Illuminate'),
