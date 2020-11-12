1bb8106e267db4f47f1e3824142488288b18b02d
M Hafizh Auliansyah <hafizh.auliansyah@gmail.com>
2020-11-13 04:13:10 +0700

    Menambahkan migration dan model untuk tabel comments

---
+++ database/migrations/2020_11_12_210449_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id("id_komentar");
+            $table->text("komentar");
+            $table->date("comment_date");
+            $table->bigInteger('nip');
+            $table->timestamps();
+
+            $table->foreign('nip')->references('nip')->on('accounts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
