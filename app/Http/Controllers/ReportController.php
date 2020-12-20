705a292de08d8c40a468b428f12e839f19d82273
M Hafizh Auliansyah <hafizh.auliansyah@gmail.com>
2020-12-20 17:02:32 +0700

    Menampilkan di tabel

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Session;
 use DB;
+use Auth;
+
 class ReportController extends Controller
 {
     public function GetReportT($jabatan){
@@ -17,7 +19,68 @@ class ReportController extends Controller
     }
     public function reportS(){
         $title = "Submission Report - ";
-        return view('contents.report-submission',['title' => $title]);
+        $jabatan = session()->get('nama_jabatan');
+        switch($jabatan){
+            case 'Kepala Sekolah':
+            case 'Admin':
+            case 'Kepala Keuangan':
+                $laporan = DB::table('submissions')
+                            ->join('transaksi','submissions.id_transaksi','=','transaksi.id_transaksi')
+                            ->join('detail_submissions','submissions.id_pengajuan','=','detail_submissions.id_pengajuan')
+                            ->join('detail_accounts','submissions.id_pengaju','=','detail_accounts.nip')
+                            ->select('submissions.*','detail_submissions.deskripsi','detail_submissions.file_lampiran','detail_accounts.nama')
+                            ->where('transaksi.jenis','!=','Pending')
+                            ->get();
+                $report = $laporan;
+                break;
+            case 'Staf BOS':
+                $laporan = DB::table('submissions')
+                            ->join('transaksi','submissions.id_transaksi','=','transaksi.id_transaksi')
+                            ->join('detail_submissions','submissions.id_pengajuan','=','detail_submissions.id_pengajuan')
+                            ->join('detail_accounts','submissions.id_pengaju','=','detail_accounts.nip')
+                            ->select('submissions.*','detail_submissions.deskripsi','detail_submissions.file_lampiran','detail_accounts.nama')
+                            ->where([
+                                'transaksi.jenis','!=','Pending',
+                                'transaksi.id_dana','=','BOS',
+                            ])
+                            ->get();
+                $report = $laporan;
+                break;
+               
+            case 'Staf APBD':
+                $laporan = DB::table('submissions')
+                            ->join('transaksi','submissions.id_transaksi','=','transaksi.id_transaksi')
+                            ->join('detail_submissions','submissions.id_pengajuan','=','detail_submissions.id_pengajuan')
+                            ->join('detail_accounts','submissions.id_pengaju','=','detail_accounts.nip')
+                            ->select('submissions.*','detail_submissions.deskripsi','detail_submissions.file_lampiran','detail_accounts.nama')
+                            ->where([
+                                'transaksi.jenis','!=','Pending',
+                                'transaksi.id_dana','=','APBD',
+                            ])
+                            ->get();
+                $report = $laporan;
+                break;
+            case 'Kaprog':
+                $laporan = DB::table('submissions')
+                            ->join('transaksi','submissions.id_transaksi','=','transaksi.id_transaksi')
+                            ->join('detail_submissions','submissions.id_pengajuan','=','detail_submissions.id_pengajuan')
+                            ->join('detail_accounts','submissions.id_pengaju','=','detail_accounts.nip')
+                            ->join('jurusan','detail_accounts.id_jurusan','=','jurusan.id_jurusan')
+                            ->select('submissions.*','detail_submissions.deskripsi','detail_submissions.file_lampiran','detail_accounts.nama','jurusan.nama_jurusan')
+                            ->where([
+                                'transaksi.jenis','!=','Pending',
+                                'submissions.id_pengaju','=',Auth::user()->nip,
+                            ])
+                            ->get();
+                $report = $laporan;
+                break;
+                
+            default:
+                abort(404);
+                break;
+        }
+
+        return view('contents.report-submission',['title' => $title,'report' => $report]);
     }
     public function reportT(){
         $title = "Transaction Report - ";
--- resources/views/contents/report-submission.blade.php
+++ resources/views/contents/report-submission.blade.php
@@ -32,63 +32,66 @@
 						<th>Status</th>
 						<th>Detail Pengajuan</th>
 					</tr>
-					<tr>
-						<td>Judul Pengajuan</td> <!-- PERLU BACKEND -->
-						<td>Nama Pengaju/Jurusan</td> <!-- PERLU BACKEND -->
-						<td>20-10-20</td> <!-- PERLU BACKEND -->
-						<td>-</td> <!-- PERLU BACKEND -->
-						<td><button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#detail">Lihat Detail</button></td>
+					@foreach ($report->all() as $r)
+						<tr>
+							<td>{{ $r->judul }}</td> <!-- PERLU BACKEND -->
+							<td>{{ $r->nama }}{{ $r->nama_jurusan? '/'.$r->nama_jurusan : '' }}</td> <!-- PERLU BACKEND -->
+							<td>{{ $r->updated_at }}</td> <!-- PERLU BACKEND -->
+							<td>{{ $r->status }}</td> <!-- PERLU BACKEND -->
+							<td><button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#detail">Lihat Detail</button></td>
 
-					<!-- Modal -->
-					<div class="modal" id="detail">
-						<div class="modal-dialog">
-							<div class="modal-content">
-								
-								<!-- Header --->
-								<div class="modal-header">
-									<h4 class="modal-title">Judul Pengajuan</h4> <!-- DI GET DARI DATA PENGAJU -->
-									<button type="button" class="close" data-dismiss="modal">&times;</button>
-								</div>
+						<!-- Modal -->
+						<div class="modal" id="detail">
+							<div class="modal-dialog">
+								<div class="modal-content">
+									
+									<!-- Header --->
+									<div class="modal-header">
+										<h4 class="modal-title">{{ $r->judul }}</h4> <!-- DI GET DARI DATA PENGAJU -->
+										<button type="button" class="close" data-dismiss="modal">&times;</button>
+									</div>
 
-								<!-- Body -->
-								<div class="modal-body top"> <!-- DI GET DARI DATA PENGAJU --> <!-- DESKRIPSI -->
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+									<!-- Body -->
+									<div class="modal-body top"> <!-- DI GET DARI DATA PENGAJU --> <!-- DESKRIPSI -->
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
-									<div class="modal-body bottom">
-										<label><b>File Lampiran</b></label>
-											<!-- CANTUMKAN FILE LAMPIRAN -->
+										<div class="modal-body bottom">
+											<label><b>File Lampiran</b></label>
+												<!-- CANTUMKAN FILE LAMPIRAN -->
+										</div>
+										<div class="modal-body bottom">
+											<label><b>Status : </b></label>
+											<label>-</label> <!-- CANTUMKAN STATUS -->
+										</div>
+										<div class="modal-body bottom">
+											<label><b>Id Transaksi : </b></label>
+											<label>-</label> <!-- MUNCUL JIKA PENGAJUAN DITERIMA/ACC -->
+										</div>
+										<div class="modal-body bottom">
+											<label><b>Pengaju : </b></label>
+											<label>Nama Pengaju</label> <!-- CANTUMKAN NAMA PENGAJU -->
+										</div>
+										<div class="modal-body bottom">
+											<label><b>Tanggal Diajukan : </b></label>
+											<label>12-12-20 </label> <!-- CANTUMKAN TAANGGAL DIAJUKAN -->
+										</div>
+										<div class="modal-body bottom">
+											<img src="../img/icon/stm.png" class="ava" alt="">&nbsp; <!-- GET AVATAR PENGOMENTAR-->
+											<label>Nama Pengomentar</label> <br><br> <!-- GET NAMA PENGOMENTAR-->
+											<textarea disabled="" class="form-control"></textarea> <!-- GET KOMENTAR-->
+										</div>
 									</div>
-									<div class="modal-body bottom">
-										<label><b>Status : </b></label>
-										<label>-</label> <!-- CANTUMKAN STATUS -->
-									</div>
-									<div class="modal-body bottom">
-										<label><b>Id Transaksi : </b></label>
-										<label>-</label> <!-- MUNCUL JIKA PENGAJUAN DITERIMA/ACC -->
-									</div>
-									<div class="modal-body bottom">
-										<label><b>Pengaju : </b></label>
-										<label>Nama Pengaju</label> <!-- CANTUMKAN NAMA PENGAJU -->
-									</div>
-									<div class="modal-body bottom">
-										<label><b>Tanggal Diajukan : </b></label>
-										<label>12-12-20 </label> <!-- CANTUMKAN TAANGGAL DIAJUKAN -->
-									</div>
-									<div class="modal-body bottom">
-										<img src="../img/icon/stm.png" class="ava" alt="">&nbsp; <!-- GET AVATAR PENGOMENTAR-->
-										<label>Nama Pengomentar</label> <br><br> <!-- GET NAMA PENGOMENTAR-->
-										<textarea disabled="" class="form-control"></textarea> <!-- GET KOMENTAR-->
-									</div>
-								</div>
 
-								<!-- Footer -->
-								<div class="modal-footer">
-									<button type="button" class="btn btn-success" data-dismiss="modal">OK</button>
+									<!-- Footer -->
+									<div class="modal-footer">
+										<button type="button" class="btn btn-success" data-dismiss="modal">OK</button>
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-					</tr>
+						</tr>
+					@endforeach
+					
 					</table>
 				</div>
 			</div>
