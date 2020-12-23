6a999f2b298b3e2de13b963126bb0c62fd1ebbd8
Septian Yoga <septianpermadi0709@gmail.com>
2020-12-23 15:38:43 +0700

    Membagi layout (if if)  report (submission & transaksi)

--- resources/views/contents/report-transaksi.blade.php
+++ resources/views/contents/report-transaksi.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.layout-report')
 
 @section('sub-content')
-    <div class="content">
+    @if() <!--Jabatan = Kepsek, Ka. Keuangan--> 
+	<div class="content">
 		<div class="back">
 			<a href="/report"><i class="fa fa-arrow-left" title="Back to Report"></i></a>
 		</div>
@@ -72,4 +73,82 @@
 		<!--Tabel Transaksi End-->
 
 	</div>
+	@endif
+
+	@if() <!--Jabatan = Staf APBD, Staf BOS-->
+	<div class="content">
+		<div class="back">
+			<a href="/report"><i class="fa fa-arrow-left" title="Back to Report"></i></a>
+		</div>
+		<div class="header_report">
+			<h4>Laporan Transaksi</h4>
+		</div> <br>
+		<!-- Row Start-->
+		<div class="row">
+			<div class="col-lg-6 col-xs-6">
+				<div class="small-box bg-primary">
+					<div class="inner">
+						<h2>100</h2> <!--GET COUNT JUMLAH PADA TABEL, JENIS: PEMASUKAN-->
+						<p>Pemasukan</p>
+					</div>
+					<div class="ikon">
+						<i class="fas fa-file-download"></i>
+					</div>
+					<a href="#tabel-transaksi" class="small-box-footer">View More <i class="fas fa-arrow-circle-right"></i></a>
+				</div>
+			</div>
+			<div class="col-lg-6 col-xs-6">
+				<div class="small-box bg-danger">
+					<div class="inner">
+						<h2>100</h2> <!--GET COUNT JUMLAH PADA TABEL, JENIS: PENGELUARAN-->
+						<p>Pengeluaran</p>
+					</div>
+					<div class="ikon">
+						<i class="fas fa-file-upload"></i>
+					</div>
+					<a href="#tabel-transaksi" class="small-box-footer">View More <i class="fas fa-arrow-circle-right"></i></a>
+				</div>
+			</div>
+		</div>
+		<!-- Row End-->
+
+		<!--Tabel Transaksi Start-->
+		<div class="box1 box-info" id="tabel-transaksi">
+			<div class="box-header with-border">
+			@if() <!--Jabatan = Staf APBD-->
+				<h3 class="box-title">Tabel Transaksi APBD</h3>
+			@endif
+
+			@if() <!--Jabatan = Staf BOS-->
+				<h3 class="box-title">Tabel Transaksi BOS</h3>
+			@endif
+			</div>
+			<div class="box-info">
+				<div class="table-responsive">
+					<table class="table no-margin"> 
+						<!-- GET DARI DATABASE, HANYA TAMPILKAN SESUAI JENIS DANA JIKA BOS = HANYA TAMPILKAN BOS, JIKA APBD = HANYA TAMPILKAN APBD -->
+						<thead>
+							<tr>
+								<th>Jumlah</th>
+								<th>Jenis</th>
+								<th>Pengaju</th>
+								<th>Tanggal Transaksi</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>Rp. 1.000.000</td>
+								<td>Keluar/Masuk/Pending</td>
+								<td>Pengaju</td>
+								<td>20-10-20</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+		<!--Tabel Transaksi End-->
+
+	</div>
+	@endif
 @endsection
--- resources/views/contents/report.blade.php
+++ resources/views/contents/report.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.layout-report')
 
 @section('sub-content')
+	@if() <!--Jabatan = Kepsek, Ka. Keuangan, Staf APBD, Staf BOS -->
     <div class="content">
 		<div class="row">
   		<div class="col-sm-6">
@@ -10,6 +11,9 @@
   				<a href="/report-transaction" class="btn btn-primary">Check</a>
   			</div>
   		</div>
+	@endif
+	
+	@if() <!--(Semua) Jabatan = Kepsek, Ka. Keuangan, Kaprog, Staf APBD, Staf BOS -->
   		<div class="col-sm-6">
   			<div class="thumbnail">
   				<h5>Laporan Pengajuan</h5>
@@ -19,4 +23,5 @@
   		</div>
 		</div>
 	</div>
+	@endif
 @endsection
