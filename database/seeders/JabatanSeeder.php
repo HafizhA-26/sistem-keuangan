21669adb6ed94576b7fe5e3c3cbc6897dc068a98
M Hafizh Auliansyah <hafizh.auliansyah@gmail.com>
2021-01-11 16:40:50 +0700

    Menambahkan comment di jabatan seeder

--- database/seeders/JabatanSeeder.php
+++ database/seeders/JabatanSeeder.php
@@ -17,15 +17,29 @@ class JabatanSeeder extends Seeder
     public function run()
     {
         $jabatan = ['Admin','Kepala Sekolah','Kepala Keuangan','Staf BOS','Staf APBD','Kaprog'];
+
+        // Id templatenya
         $id = 'J000';
+
+        // Untuk increment id
         $counter = 0;
+
         foreach($jabatan as $namaJ){
             Jabatan::create([
                 'id_jabatan'    =>  $id,
                 'nama_jabatan'  =>  $namaJ
             ]);
+            // counter ditambah 1
             $counter++;
+
+            // ngambil panjang karakter $counter
             $counterlen = strlen((string)$counter);
+
+            // mengganti beberapa karakter dari variabel $id dengan value $counter, jumlah karakter yang diganti sesuai panjang karakter $counter
+            // Penjelasan subster_replace
+            // Parameter 1 : variabel string yang mau diganti karakternya , disini diisi dengan template id
+            // Parameter 2 : variabel string pengganti karakter, disini diisi penghitung/counter dari id
+            // Parameter 3 : jumlah & letak karakter dari parameter 1 yang mau diganti dengan parameter 2. Kalau parameter ini negative artinya, proses penggantian karakter dimulai dari belakang
             $id = substr_replace($id,(string)$counter,$counterlen*-1);
 
         }
